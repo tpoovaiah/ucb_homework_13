1269ed6803701534d805bd0461aa5e3f2f1d660b
tanyap <biddanda.tanya@gmail.com>
2020-07-07 13:56:04 -0700

    adds all necessary files and framework

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chicken Katsu Burger", true), ("Black Bean Fiesta Burger", false), ("Hawaiian Burger", false), ("Double Bacon Cheeseburger", false), ("Luther Burger", false)
