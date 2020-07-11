01a05565eba0533a26c3d19ee52646f6cea11b77
tanyap <biddanda.tanya@gmail.com>
2020-07-11 13:37:30 -0700

    adds JAWSDB info

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,26 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Daruma340912",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: 'Daruma340912',
+    database: "burgers_db"
+  })
+}
+
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Daruma340912",
+//   database: "burgers_db"
+// });
 
 connection.connect(function(err) {
   if (err) {
